a774b73be04eb9d050bf07df671915609d3fd748
skyrosoft <125123066+skyrosoft@users.noreply.github.com>
2023-04-03 18:38:35 +0530

    Add files via upload

---
+++ Company - 01-Apr-2023/scroll.js
@@ -0,0 +1,27 @@
+const scrollbtn = document.querySelector('.btn');
+
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+  
+    if (window.scrollY > 100) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    }); 
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
---
+++ Company - 01-Apr-2023/slider.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+  $('.slider').slick({
+    
+  autoplay:true,
+  autoplaySpeed:1000,
+  arrows:true,
+  prevArrow:'<button type="button" class="slick-prev"></button>',
+  nextArrow:'<button type="button" class="slick-next"></button>',
+  centerMode:true,
+  slidesToShow:3,
+  slidesToScroll:1
+  });
+};
